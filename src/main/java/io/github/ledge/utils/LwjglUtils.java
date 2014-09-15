f85a2351e8b0ebbd25d0ea241876d08c899cd14f
CaptainBern <captainberneby@gmail.com>
2014-09-16 00:43:55 +0200

    Added lwjgl utils + temp natives dir (needs a fix)

--- src/main/java/io/github/ledge/utils/LwjglUtils.java
+++ src/main/java/io/github/ledge/utils/LwjglUtils.java
@@ -1,14 +1,119 @@
 package io.github.ledge.utils;
 
+import com.google.common.collect.Lists;
+import org.lwjgl.LWJGLUtil;
+
+import java.io.*;
+import java.lang.reflect.Field;
+import java.net.URL;
 import java.nio.file.Path;
+import java.util.List;
 
 public class LwjglUtils {
 
-    public static void intializeLwjgl() {
+    private static final String NATIVES_EXPORT_DIR = "./natives";
 
+    private static final String NATIVES = "natives/";
+    private static final String WINDOWS = NATIVES + "windows/";
+    private static final String LINUX = NATIVES + "linux/";
+    private static final String MAC_OSX = NATIVES + "mac/";
+
+    public static void intializeLwjgl() {
+        identifyPlatform();
     }
 
-    private static void setLibPath(Path path) {
+    private static void identifyPlatform() {
+        switch (LWJGLUtil.getPlatform()) {
+            case LWJGLUtil.PLATFORM_WINDOWS:
+                unpack(WINDOWS + "OpenAL64.dll");
+                unpack(WINDOWS + "lwjgl64.dll");
+                unpack(WINDOWS + "jinput-dx8_64.dll");
+                unpack(WINDOWS + "jinput-raw_64.dll");
 
+                unpack(WINDOWS + "OpenAL32.dll");
+                unpack(WINDOWS + "lwjgl.dll");
+                unpack(WINDOWS + "jinput-dx8.dll");
+                unpack(WINDOWS + "jinput-raw.dll");
+
+                unpack(WINDOWS + "jinput-wintab.dll");
+                break;
+            case LWJGLUtil.PLATFORM_LINUX:
+                unpack(LINUX + "libopenal64.so");
+                unpack(LINUX + "liblwjgl64.so");
+                unpack(LINUX + "libjinput-linux64.so");
+
+                unpack(LINUX + "libopenal32.so");
+                unpack(LINUX + "liblwjgl32.so");
+                unpack(LINUX + "libjinput-linux.so");
+                break;
+            case LWJGLUtil.PLATFORM_MACOSX:
+                unpack(MAC_OSX + "liblwjgl.jnilib");
+                unpack(MAC_OSX + "libopenal.dylib");
+                unpack(MAC_OSX + "libjinput-osx.jnilib");
+                break;
+            default:
+                throw new RuntimeException("Failed to identify the platform!");
+        }
+
+        addLibPath(new File(NATIVES_EXPORT_DIR).toPath());
+    }
+
+    private static void unpack(String resource) {
+        try {
+            File exportDir = new File(NATIVES_EXPORT_DIR);
+            exportDir.mkdirs();
+            exportDir.deleteOnExit();
+            File libFile = new File(NATIVES_EXPORT_DIR, resource.substring(resource.lastIndexOf('/'), resource.length()));
+
+            if (!libFile.exists()) {
+                libFile.deleteOnExit();
+
+                InputStream inputStream = ClassLoader.getSystemResourceAsStream(resource);
+                OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(libFile));
+
+                int len;
+                byte[] buffer = new byte[8192];
+
+                while ((len = inputStream.read()) > -1) {
+                    outputStream.write(buffer, 0, len);
+                }
+
+                outputStream.flush();
+                outputStream.close();
+                inputStream.close();
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to copy the natives to the right directory");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private static void addLibPath(Path path) {
+        String envProp = System.getProperty("java.library.path");
+
+        if (envProp == null || envProp.isEmpty()) {
+            System.setProperty("java.library.path", path.toAbsolutePath().toString());
+        } else {
+            System.setProperty("java.library.path", path.toAbsolutePath().toString() + File.separator + envProp);
+        }
+
+        try {
+            Field usrPaths = ClassLoader.class.getDeclaredField("usr_paths");
+            usrPaths.setAccessible(true);
+
+            List<String> paths = Lists.newArrayList((String[]) usrPaths.get(null));
+
+            if (paths.contains(path.toAbsolutePath().toString()))
+                return;
+
+            paths.add(path.toAbsolutePath().toString());
+
+            usrPaths.set(null, paths.toArray(new String[paths.size()]));
+        } catch (Exception e) {
+            System.err.println("Failed to set the native libraries");
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
